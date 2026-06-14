a2cffb10341e679c9c6ed2d9b7b972c4a17eddd2
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-06-14 18:02:19 +0530

    server setup completed

---
+++ server/controllers/auth/authController.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User');
+
+
+
+
+const registerUser = async(req,res) => {
+    const { userName, email, password} = req.body;
+    try {
+        const hashPassword = await bcrypt.hash(password, 12);
+        const newUser = new User({
+            userName,
+            email,
+            password: hashPassword,
+
+        });
+
+        await newUser.save()
+        res.status(200).json({
+            success: true,
+            message: "registration successful",
+        })
+
+    }catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: "some error occured",
+        })
+    }
+};
+
+const login = async(req,res) => {
+       try {
+
+    }catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: "some error occured",
+        })
+    }
+}
+
+module.exports = { registerUser }
+
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const  authRouter = require('./routes/auth-routes')
 
 // mongoose.connect('mongodb+srv://vindhyalakshmiofficial_db_user:vindhya1@cluster0.eccgabx.mongodb.net/')
 // const uri = 'mongodb+srv://vindhyalakshmiofficial_db_user:vindhya1@cluster0.eccgabx.mongodb.net/';
@@ -36,5 +37,6 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
