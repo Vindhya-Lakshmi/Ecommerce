61abfaa7822fc2bd4cbcf38a7b3438b710587696
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-07-09 21:15:02 +0530

    shoping list

--- client/src/components/shopping-view/filter.jsx
+++ client/src/components/shopping-view/filter.jsx
@@ -1,3 +1,4 @@
+import {  Checkbox, Label, Separator } from "radix-ui"
 import { Fragment } from "react"
 
 
@@ -13,7 +14,16 @@ function ProductFilter() {
                     <Fragment>
                         <div>
                             <h3 className="text-base font-bold">{keyItem}</h3>
+                            <div className="grid gap-2 mt-2">
+                                {
+                                    filterOptions[keyItem].map(option=> <Label className="flex font-medium items-center gap-2 ">
+                                        <Checkbox/>
+                                        {option.label}
+                                    </Label>)
+                                }
+                            </div>
                         </div>
+                        <Separator/>
                     </Fragment>
                     ))}
             </div>
--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,10 +1,18 @@
+import ProductFilter from "@/components/shopping-view/filter"
 
 
 
 
 function ShoppingListing() {
     return
-        <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6"></div>
+        <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+            <ProductFilter/>
+            <div className="bg-background w-full rounded-lg shadow-sm">
+                <div className="p-4 border-b flex items-center justify-between">
+                    <h2 className="text-lg font-extrabold">All Products</h2>
+                </div>
+            </div>
+        </div>
     
 }
 
