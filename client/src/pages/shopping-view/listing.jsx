04c77967e33a513f6a650fce3396ff832fe93f42
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-07-12 18:21:15 +0530

    product-tile

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -34,6 +34,9 @@ function ShoppingListing() {
                 </div>
 
             </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+
+            </div>
         </div>
     </div>
 
