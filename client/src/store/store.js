7377f81db2874a9f02d7be8c6ec7e3d692b91f22
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-08-13 21:58:38 +0530

    lgfvbk

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,26 +1,27 @@
 import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
+import adminOrderSlice from "./admin/order-slice";
 import shopProductSlice from "./shop/product-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
 import shopReviewSlice from "./shop/review-slice";
 import commonFeatureSlice from "./common-slice";
-import shopOrderReducer from "./shop/order-slice";
-import adminOrderReducer from "./admin/order-slice";
+// import shopOrderReducer from "./shop/order-slice";
+// import adminOrderReducer from "./admin/order-slice";
 import shopOrderSlice from "./shop/order-slice"
 
 const store = configureStore({
     reducer : {
         auth : authReducer,
         adminProducts : adminProductsSlice,
+        adminOrder : adminOrderSlice,
         shopProducts : shopProductSlice,
         shopCart: shopCartSlice,
         shopAddress: shopAddressSlice,
         shopReview: shopReviewSlice,
         commonFeature: commonFeatureSlice,
-        shopOrder: shopOrderReducer,
-        adminOrder: adminOrderReducer,
+        shopOrder: shopOrderSlice,
         shopOrderSlice: shopOrderSlice,
     }, 
     
--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes")
+const adminOrderRouter = require("./routes/admin/order-routes")
+
 const shopProductsRouter = require('./routes/shop/products-routes');
 const shopCartRouter = require('./routes/shop/cart-routes');
 const shopAddressRouter = require('./routes/shop/address-routes');
 const shopOrderRouter = require("./routes/shop/order-routes");
+
 
 // mongoose.connect('mongodb+srv://vindhyalakshmiofficial_db_user:vindhya1@cluster0.eccgabx.mongodb.net/')
 // const uri = 'mongodb+srv://vindhyalakshmiofficial_db_user:vindhya1@cluster0.eccgabx.mongodb.net/';
@@ -38,6 +41,8 @@ app.use(express.json());
 
 app.use('/api/auth', authRouter);
 app.use ("/api/admin/products",adminProductsRouter)
+app.use ("/api/admin/orders",adminOrderRouter)
+
 app.use("/api/shop/products", shopProductsRouter)
 app.use("/api/shop/cart", shopCartRouter)
 app.use("/api/shop/address", shopAddressRouter)
