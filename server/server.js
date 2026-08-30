43644e34f0c5d4c3ed2ff284581240ca8797ee5b
Vindhya <vindhyalakshmiofficial@gmail.com>
2026-08-30 19:29:02 +0530

    review page

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const shopCartRouter = require('./routes/shop/cart-routes');
 const shopAddressRouter = require('./routes/shop/address-routes');
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopShopRouter = require("./routes/shop/shop-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
 
 
 // mongoose.connect('mongodb+srv://vindhyalakshmiofficial_db_user:vindhya1@cluster0.eccgabx.mongodb.net/')
@@ -49,5 +50,6 @@ app.use("/api/shop/cart", shopCartRouter)
 app.use("/api/shop/address", shopAddressRouter)
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopShopRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
